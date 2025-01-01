77978b7cdbd2e5dcf01160f15c60972ad1c8c0ea
MarcusLucena <marcus-lucena@outook.com>
2025-01-01 07:31:52 -0300

    Criando a lista e cadastro de tarefas

--- main.go
+++ main.go
@@ -7,6 +7,16 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
+type Tasks struct {
+	Id    int    `json:"id"`
+	Title string `json:"title"`
+}
+
+var taskList = []Tasks{
+	{Id: 891, Title: "Estudar Go Lang"},
+	{Id: 123, Title: "Seguir o Sujeito Programador no Youtube"},
+}
+
 func main() {
 	fmt.Print("Minha primeira API em GO")
 
@@ -14,10 +24,31 @@ func main() {
 
 	router.SetTrustedProxies(nil)
 
-	router.GET("/", func(c *gin.Context) {
-		c.JSON(http.StatusOK, gin.H{
+	router.GET("/", func(ctx *gin.Context) {
+		ctx.JSON(http.StatusOK, gin.H{
 			"message": "Primeira API em Go",
 		})
+	})
+
+	// Lista todas tarefas
+	router.GET("/tarefas", func(ctx *gin.Context) {
+		ctx.IndentedJSON(http.StatusOK, taskList)
+	})
+
+	// Cadastrar tarefa
+	router.POST("/tarefas", func(ctx *gin.Context) {
+		var newTask Tasks
+
+		if err := ctx.BindJSON(&newTask); err != nil {
+			ctx.JSON(http.StatusBadRequest, gin.H{
+				"error": err.Error(),
+			})
+			return
+		}
+
+		newTask.Id = len(taskList) + 1
+		taskList = append(taskList, newTask)
+		ctx.JSON(http.StatusOK, newTask)
 	})
 
 	router.Run(":3000")
